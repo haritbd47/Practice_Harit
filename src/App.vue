445e92cbec22b71e6047e5e64c92499fb56d5af2
haritbd47 <132350040+haritbd47@users.noreply.github.com>
2023-05-05 14:03:11 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>,
+    <router-link to="/vmodel">/Vmodel</router-link>,
+    <router-link to="/von">/V-On</router-link>,
+    <router-link to="/vbind">/V-Bind</router-link>,
+    <router-link to="/prop">/Prop</router-link>, <br /><br />
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
